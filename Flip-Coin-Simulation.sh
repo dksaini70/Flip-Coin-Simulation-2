2354ebe3f45994e9206d35a6d2aba71a608205ea
dksaini70 <dkreview70@gmail.com>
2021-06-27 19:29:10 +0530

    singlet combination and its percentage

--- Flip-Coin-Simulation.sh
+++ Flip-Coin-Simulation.sh
@@ -1,11 +1,27 @@
+#!/bin/bash 
 echo "find head or tail"
 Head=1
 Tail=0
+count=0
+count1=0
+n=10
+declare -A store
+for(( i=1;i<=n;i++ ))
+do
 coin=$((RANDOM%2))
 if [ $coin -eq $Head ]
 then 
-echo "head"
+store[$i]="head"
+((count++))
 else
-echo "tail"
+store[$i]="tail"
+((count1++))
 fi
-
+done
+echo ${store[@]}
+echo $count
+echo $count1
+percent=$(( $count*100 /$n ))
+echo "percent of head "$percent"%"
+percent1=$(($count1 * 100 /$n))
+echo "percent of tail "$percent1"%"
